A tiny fix which allows stacked gradients. TODO: take care of the plotting

examples/quickstart.py
@@ -3,9 +3,16 @@ from mriseqplot.core import SeqDiagram
 from mriseqplot.shapes import trapezoid
 
 
-t = np.linspace(0, 6, 100)
+t = np.linspace(0, 6, 100)[:, None]  # needed for broadcasting later
 sequence = SeqDiagram(t, ["PEG", "FEG"])
-sequence.add_element("PEG", trapezoid, ampl=2, t_start=1, t_flat_out=1, t_ramp_down=2)
+sequence.add_element(
+    "PEG",
+    trapezoid,
+    ampl=np.array([1, 1.5])[None, :],  # some broadcasting magic for stacked gradients
+    t_start=1,
+    t_flat_out=1,
+    t_ramp_down=2,
+)
 sequence.add_element(
     "FEG", trapezoid, ampl=-1, t_start=2, t_flat_out=2.2, t_ramp_down=2.8
 )

mriseqplot/core.py
@@ -45,7 +45,7 @@ class SeqDiagram:
         overlap = np.logical_and(self.axes[axis_name], unit)
         if overlap.any():
             warnings.warn(f"Got an overlap in {axis_name} using {callback.__name__}")
-        self.axes[axis_name] += unit
+        self.axes[axis_name] = self.axes[axis_name] + unit
 
     def plot_scheme(self):
         """ Plot the sequence diagram """