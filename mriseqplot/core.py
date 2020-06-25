fd33afb3a94142da4fda81c294f01e4297019d61
Sibgatulin <renat.sibgatulin@gmail.com>
2020-06-25 18:57:01 +0200

    Split Sequence into Channel and Diagram classes. One for the numerics and the other is for plotting. Importantly, now ax2channel maps to channels themselves, not to channel names. Arguable, no one needs channel names. Except for styles, of course. But we are addressing it, right? 😜 Now, most of what was in plot.py is in Diagram, although I am not entirely sold on it. Colour specification is a bit clumsy now, ideas are welcome. rcParams is not tested and I have very little clue how it works now, honestly. That's to be done. On the idea of being able to *declare* annotations first and then plot the whole thing, one can consider adding them to Diagram 🤔

--- mriseqplot/core.py
+++ mriseqplot/core.py
@@ -1,46 +1,34 @@
 import numpy as np
+import matplotlib as mpl
 import matplotlib.pyplot as plt
-from typing import Callable, List
-from mriseqplot.plot import _format_axes_base, _project_channel_on_time_axis, rcParams
+import matplotlib.transforms as transforms
+from typing import Callable
+from mriseqplot.plot import _project_channel_on_time_axis, rcParams
 
 
-class Sequence:
-    """ Class to define and collect sequences of events (e.g gradient wavefronts)
+class Channel:
+    """ Class to define and collect sequences of events (e.g. gradient wavefronts)
     that all share the same time axis. This class is not meant to handle the job of
-    representation of the events (refered to as channels).
+    representation of the events (aptly referred to as channels).
     """
 
-    def __init__(self, t, channels: List[str]):
-        """ Initialize sequence diagram
+    def __init__(self, t):
+        """ Initialize a sequence of events
         Parameters
         ----------
         t : np.array, 1D
             Sets the grid for all waveforms to be computed on
-        channels : list of strings
-            Names of the individual lines of the diagram. Example:
-            ["RF", "PEG", "FEG", "SSG"]. As of now the order defines the order in which
-            the lines will appear on the plot.
-        ax2channel : dict, optional
-            Defines the layout of the desired diagram.
-            Maps from subplots (axes) represented by their labels to channels.
-            If not given, every channel will be plotted in its own subplot and channel
-            name will be used as subplots's ylabel.
-
-        Axes are represented as a dictionary with axes names being the keys. Upon
-        initialization all waveforms set to zero-filled arrays the same length as t.
         """
-        self.t = t
-        self.channels = {}
-        for channel in channels:
-            self.channels[channel] = np.full_like(t, np.nan, dtype=float)
+        # does not copy data, assigns a pointer
+        self.time = t
+        self.data = np.full_like(t, np.nan, dtype=float)
+        # a little field which helps to represent and identify the channel
+        self._elements = []
 
-    def add_element(self, channel_name: str, callback: Callable, ampl=1, **kwargs):
+    def add_element(self, callback: Callable, ampl=1, **kwargs):
         """ Generic function to add an element to a waveform
         Parameters
         ----------
-        channel_name : str
-            Name of the axis to add an element to. Will raise a KeyError if the name
-            was not given upon initialization
         callback : callable
             Function to compute the (unit amplitude) waveform of the element.
             Signature: fun(self.t, **kwargs)
@@ -48,67 +36,86 @@ class Sequence:
             Desired amplitude of the element. Default: 1
         **kwargs : dict
             All keyword arguments which will be passed to the callback
-
-        The function tests if the new waveform overlaps with anything present in the
-        chosen axis and issues a warning if it does
         """
-        unit = ampl * callback(self.t, **kwargs)
+        unit = ampl * callback(self.time, **kwargs)
 
         # Stacking along a new dim needs explicit broadcasting if arrays mismatch
-        tmp_stack = np.stack(
-            np.broadcast_arrays(self.channels[channel_name], unit), axis=-1
-        )
+        tmp_stack = np.stack(np.broadcast_arrays(self.data, unit), axis=-1)
         # np.nan + anything is np.nan -> use nansum.
         sum_no_nan = np.nansum(tmp_stack, axis=-1)
         # Caveat: nansum substitutes all np.nan for 0s, which is undesirable
         # -> need to keep nans in both arrays manually
         both_nan = np.isnan(tmp_stack).all(axis=-1)
-        self.channels[channel_name] = np.where(both_nan, np.nan, sum_no_nan)
+        self.data = np.where(both_nan, np.nan, sum_no_nan)
+        self._elements.append(callback.__name__)
+        return self
+
+    def __repr__(self):
+        return f"channel with {self._elements}"
 
 
 class Diagram:
     """ Class to map a sequence of events on a figure and to handle the representation
     """
 
-    def __init__(self, sequence, ax2channel=None):
-        """
+    def __init__(self, ax2channel):
+        """ Map channels to axes on a figure
+
         Parameters
         ----------
-        sequence : Sequence
-        ax2channel : dict, optional
+        ax2channel : dict
             Defines the layout of the desired diagram.
             Maps from subplots (axes) represented by their labels to channels.
-            If not given, every channel will be plotted in its own subplot and channel
-            name will be used as subplots's ylabel.
         """
-        self.seq = sequence
-        if ax2channel is None:
-            # trivial map
-            self.ax_names = [name for name in sequence.channels.keys()]
-            self.channel_names = [name for name in sequence.channels.keys()]
-        else:  # convert single labels to iterables
-            self.ax_names = [ax_name for ax_name in ax2channel.keys()]
-            self.channel_names = [
-                [ch] if isinstance(ch, str) else ch for ch in ax2channel.keys()
-            ]
+        # convert single channels to iterables
+        self.ax2ch = {
+            k: [ch] if isinstance(ch, Channel) else ch for k, ch in ax2channel.items()
+        }
         self.fig, self.axes = plt.subplots(
-            nrows=len(self.ax_names), sharex=True, sharey=True
+            nrows=len(self.ax2ch), sharex=True, sharey=True
         )
+        self.channels = [ch for chs in self.ax2ch.values() for ch in chs]
+        # test how it works if len(self.channels) == 1
+        # Potentially makes sense to use np.allclose(self.channels[0].time, ch.time)
+        # but I compare not just the equivalence but actually being the same array 🤷
+        if not all([self.channels[0].time is ch.time for ch in self.channels[1:]]):
+            raise ValueError("Not all of the given channels share the same time axis")
+        self.t = self.channels[0].time
 
-    def plot_scheme(self):
-        """ Plot the sequence diagram """
+    def plot_scheme(self, colors=None, alpha=0.3, time_axis_on_top=False):
+        """ Plot the sequence diagram
+
+        Parameters
+        ----------
+        colors : iterable, optional
+            Iterable of valid matplotlib colors for every channel. If not given, default
+            matplotlib's color cycle is used (i.e. Tableau Colors from the 'tab10'
+            categorical palette)
+        alpha : float, optional
+            Global alpha channel value for the filling under every channel's profile
+        time_axis_on_top : bool, optional
+            If to draw the time axis on top of the waveforms, or only where no event
+            present. Default: False
+        """
+
+        if colors is None:
+            colors = [f"C{idx}" for idx in range(len(self.channels))]
+        if len(colors) != len(self.channels):
+            raise ValueError("If given, colors must match total number of channels")
+        color_cycle = colors[::-1]  # reversed copy
+
         # transAxes is easier to use when axes do not have arbitrary offset between
         plt.subplots_adjust(hspace=0)
         self._format_axes_base()
         self._format_axes_data()
-        for ax, (label, channels) in zip(self.axes, self.ax2channel.items()):
-            for name_channel in channels:
-                self._plot_channel(ax, name_channel)
+        for ax, channels in zip(self.axes, self.ax2ch.values()):
+            for channel in channels:
+                self._plot_channel(ax, channel, color=color_cycle.pop(), alpha=alpha)
+        self._plot_time_axes(time_axis_on_top)
 
     def _format_axes_base(self):
         """ Formats the axes according to the selected style.
             Only performs data independent formatting """
-
         for ax in self.axes:
             ax.set_yticks([])
             if not rcParams["axes_ticks"]:
@@ -120,16 +127,16 @@ class Diagram:
                 ax.spines[side].set_visible(False)
 
     def _format_axes_data(self, padding_factor=1.1):
-
+        """ Data specific axes formatting, such as ylim setting and ylabels """
         # set consistent y-limit as maximum from all plots
         ylim = [0.0, 0.0]
-        for signal in self.seq.channels.values():
-            ylim[0] = min(ylim[0], padding_factor * np.nanmin(signal))
-            ylim[1] = max(ylim[1], padding_factor * np.nanmax(signal))
+        for signal in self.channels:
+            ylim[0] = min(ylim[0], padding_factor * np.nanmin(signal.data))
+            ylim[1] = max(ylim[1], padding_factor * np.nanmax(signal.data))
         for ax in self.axes:
             ax.set_ylim(ylim)
 
-        for ax, ax_name in zip(self.axes, self.ax_names):
+        for ax, ax_name in zip(self.axes, self.ax2ch.keys()):
             ax.set_ylabel(
                 ax_name,
                 rotation=0,
@@ -138,13 +145,26 @@ class Diagram:
                 multialignment="center",
             )
 
-    def _plot_time_axes(self):
-        for ax, names_channel_per_ax in zip(self.axes, self.channel_names):
-            channels_per_ax = [self.seq.channels[k] for k in names_channel_per_ax]
-            axis = _project_channel_on_time_axis(channels_per_ax)
+    def _plot_time_axes(self, time_axis_on_top=False):
+        """ Plot time axes for each axis
+
+        Parameters
+        ----------
+        time_axis_on_top : bool, optional
+            If True, the time axis will be drawn on top of the channels,
+            otherwise will only be visible where no waveform is present
+
+        """
+        for ax, channels in zip(self.axes, self.ax2ch.values()):
+            if time_axis_on_top:
+                axis = np.zeros_like(self.t)
+            else:
+                signals = [ch.data for ch in channels]
+                axis = _project_channel_on_time_axis(signals)
+
             # color needs fixing
             ax.plot(
-                self.seq.t, axis, color=mpl.rcParams["axes.edgecolor"],
+                self.t, axis, color=mpl.rcParams["axes.edgecolor"],
             )
             ax.arrow(
                 x=self.t[-1],
@@ -158,34 +178,61 @@ class Diagram:
                 # clip_on=False, # why exactly?
             )
 
-    def _plot_channel(self, ax, name_channel):
-        signal = self.seq.channels[name_channel]
-        style = self.axes_styles[name_channel]
-
-        # plotting of the data
-        signal_dims = signal.shape
-        for dim in range(signal_dims[1]):
-            plt_signal = signal[:, dim]
-
+    def _plot_channel(self, ax, channel, color, alpha=0.3):
+        """ Plot given channel on a given axis with given color, simple, huh? """
+        # That  needs to be tested against channels of shape (n,). How did it work?
+        for signal in channel.data.T:
             ax.fill_between(
                 self.t[:, 0],
-                plt_signal,
-                0,
-                facecolor=style.color_fill,
-                # edgecolor=[0, 0, 0, 0], # isn't it such by default?
-                # do we ever want to see the line at all?
-                # linewidth=style.axes_width + style.axes_width * 0,  # excuse me?
-                # zorder=style.zorder + 5,
-                # clip_on=False, # what's the use case?
+                signal,
+                alpha=alpha,
+                facecolor=color,  # temporarily simplification
             )
-
             ax.plot(
                 self.t[:, 0],
-                plt_signal,
-                color=style.color,
+                signal,
+                color=color,
                 # linewidth=style.width, # mpl.rcParams["lines.linewidth"]
-                # clip_on=False, # what's the use case?
-                # do we need zorder here if it's plotted *after* fill_between?
-                # zorder=style.zorder + 10,  # always on top of fill
             )
-        # return ax
+
+    # I am still not sold on whether these must be methods of the class at all
+    def _plot_label(self, ax_idx, x, y, text, **kwargs):
+        self.axes[ax_idx].text(
+            x,
+            y,
+            text,
+            horizontalalignment="center",
+            verticalalignment="bottom",
+            **kwargs,
+        )
+
+    def _plot_hline(
+        self, ax_idx, xs, y, text=None, arrow_style="<|-|>", text_kws={}, **kwargs
+    ):
+        if text is not None:
+            x_label = np.mean(xs)
+            y_label = y  # why would it ever be a vector?
+            self._plot_label(ax_idx, x_label, y_label, text, **text_kws)
+
+        # the most versatile way to create all sorts of arrows
+        arrow_kws = {"arrowstyle": arrow_style, **kwargs}
+        self.axes[ax_idx].annotate(
+            "", xy=(xs[0], y), xytext=(xs[1], y), arrowprops=arrow_kws,
+        )
+
+    def _plot_vline(self, axes_idx, t, **kwargs):
+        """ Add vertical lines to specified axes
+
+        Parameters
+        ----------
+        axes_idx : iterable of integers or slice
+            Indices of the axes to draw a vertical line over
+        t : float
+            Point in time where to add a line
+        **kwargs
+            Other optional arguments are passed to plt.Line2D constructor
+        """
+        for ax in self.axes[axes_idx]:
+            trans = transforms.blended_transform_factory(ax.transData, ax.transAxes)
+            line = plt.Line2D([t, t], [0, 1], transform=trans, **kwargs)
+            ax.add_artist(line)
--- mriseqplot/plot.py
+++ mriseqplot/plot.py
@@ -1,7 +1,5 @@
 import numpy as np
 import matplotlib as mpl
-import matplotlib.pyplot as plt
-import matplotlib.transforms as transforms
 import scipy.ndimage as ndi
 
 defaults_mpl = {
@@ -21,58 +19,12 @@ rcParams = {
 # need to set up default global style
 
 
-def _plot_label(ax, x, y, text, **kwargs):
-    ax.text(
-        x, y, text, horizontalalignment="center", verticalalignment="bottom", **kwargs
-    )
-    return ax
-
-
-def _plot_hline(ax, xs, y, text=None, arrow_style="<|-|>", text_kws={}, **kwargs):
-    if text is not None:
-        x_label = np.mean(xs)
-        y_label = y  # why would it ever be a vector?
-        _plot_label(ax, x_label, y_label, text, **text_kws)
-
-    # the most versatile way to create all sorts of arrows
-    arrow_kws = {"arrowstyle": arrow_style, **kwargs}
-    ax.annotate(
-        "", xy=(xs[0], y), xytext=(xs[1], y), arrowprops=arrow_kws,
-    )
-    return ax
-
-
-def _plot_vline(axes_to_span, t, **kwargs):
-    """ Add vertical lines to specified axes
-
-    Unlike many (all) other ``add_`` methods, this method operates on axes
-    (i.e. subplots), which justifies it acting on all axes at once, rather than
-    adding individual vertical plots to each channel, as elements are added.
-    As of now it expects actual axes to work on, rather than their labels
-    (which are defined as ax2channel keys)
-
-    Parameters
-    ----------
-    axes_to_span : iterable of matplotlib's axes
-        Axes to draw a vertical line over
-    t : float
-        Point in time where to add a line
-    **kwargs
-        Other optional arguments are passed to plt.Line2D constructor
-    """
-    for ax in axes_to_span:
-        trans = transforms.blended_transform_factory(ax.transData, ax.transAxes)
-        line = plt.Line2D([t, t], [0, 1], transform=trans, **kwargs)
-        ax.add_artist(line)
-
-
 def _project_channel_on_time_axis(channels):
     """ Build a mpl.path.Path object which shows axis where all channels are empty
     Parameters
     ----------
     channels : iterable of numpy arrays of shape (n, ) or (n, m)
     """
-
     tmp_stack = np.concatenate(np.broadcast_arrays(*channels), axis=1)
     all_nan = np.isnan(tmp_stack).all(axis=1)
     all_nan_dil = ndi.binary_dilation(all_nan, np.ones(3,))
